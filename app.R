a77782f796ee53392bfed9ff89b60b40b64b5905
Max Pedersen <mape168@gmail.com>
2019-03-10 20:22:13 +0100

    added size and colour input for geomline

--- app.R
+++ app.R
@@ -16,7 +16,7 @@ library(ggplot2)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Smurf-data explorer"),
+   titlePanel("csv-data explorer"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
@@ -29,13 +29,21 @@ ui <- fluidPage(
                             ".csv")),
        ## add x-axis selector
        selectInput("x_axis", "Select x-axis", choices=NULL),
-       sliderInput("range", "Range:",
-                   min = 1750, max = 2009,
-                   value = c(1750,2009), sep = ""),
+       #sliderInput("range", "Range:",
+                   #min = 1750, max = 2009,
+                   #value = c(1750,2009), sep = ""),
        ## add y-axis selector
-       selectInput("y_axis", "Select y-axis", choices=NULL)
+       selectInput("y_axis", "Select y-axis", choices=NULL),
        ## add factor selector
        #selectInput("y_axis1", "Select y-axis1", choices=NULL)
+       numericInput("min", "x-axis minimum value", 1750),
+       numericInput("max", "x-axis maximum value", 2008),
+       numericInput("size", "Line size", 1, min = 1, max = 10),
+       selectInput("colour", "Colour",
+                   c("blue" = "blue",
+                     "green" = "green",
+                     "yellow" = "yellow",
+                     "black" = "black"))
      ),
       
      # Show a plot of the selected relationships
@@ -69,8 +77,9 @@ server <- function(input, output, session) {
     
   output$plot <- renderPlot({
     ggplot(uploaded_file(), aes_string(x = input$x_axis, y = input$y_axis)) + 
-      geom_line()+
-      xlim(input$range[1], input$range[2])+
+      geom_line(colour= input$colour, 
+                size = input$size)+
+      xlim(input$min, input$max)+
       ggtitle(input$uploadedfile$name) +
       theme_minimal()
   })
